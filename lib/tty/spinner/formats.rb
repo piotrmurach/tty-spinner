0f40ba79a7cb11f0fcb3bc3608dea5d425a52217
Piotr Murach <pmurach@gmail.com>
2016-03-13 14:30:30 +0000

    Change to expand formats and customise names.

--- lib/tty/spinner/formats.rb
+++ lib/tty/spinner/formats.rb
@@ -3,21 +3,102 @@
 module TTY
   module Formats
     FORMATS = {
-      spin_1: "|/-\\".freeze,
-      spin_2: '⎺⎻⎼⎽⎼⎻'.freeze,
-      spin_3: '◴◷◶◵'.freeze,
-      spin_4: '◐◓◑◒'.freeze,
-      spin_5: '◰◳◲◱'.freeze,
-      spin_6: '▉▊▋▌▍▎▏▎▍▌▋▊▉'.freeze,
-      spin_7: '▌▄▐▀'.freeze,
-      spin_8: '■□▪▫'.freeze,
-      spin_9: '←↑→↓'.freeze,
-      spin_10: '╫╪'.freeze,
-      spin_11: '▁▃▅▆▇█▇▆▅▃'.freeze,
-      spin_12: '◢◣◤◥'.freeze,
-      spin_13: '.oO@*'.freeze,
-      spin_14: '←↖↑↗→↘↓↙'.freeze,
-      spin_15: '-◡⊙-◠'.freeze
+      classic: {
+        frames: %w{| / - \\}
+      },
+      spin: {
+        frames: %w{◴ ◷ ◶ ◵ }
+      },
+      spin_2: {
+        frames: %w{◐ ◓ ◑ ◒ }
+      },
+      spin_3: {
+        frames: %w{◰ ◳ ◲ ◱}
+      },
+      spin_4: {
+        frames: %w{╫ ╪'}
+      },
+      pulse: {
+        frames: %w{⎺ ⎻ ⎼ ⎽ ⎼ ⎻}
+      },
+      pulse_2: {
+        frames: %w{▁ ▃ ▅ ▆ ▇ █ ▇ ▆ ▅ ▃ }
+      },
+      pulse_3: {
+        frames: '▉▊▋▌▍▎▏▎▍▌▋▊▉'
+      },
+      dots: {
+        frames: [ "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" ]
+      },
+      arrow: {
+        frames: %w{← ↖ ↑ ↗ → ↘ ↓ ↙ }
+      },
+      arrow_pulse: {
+        frames: [
+          "▹▹▹▹▹",
+          "▸▹▹▹▹",
+          "▹▸▹▹▹",
+          "▹▹▸▹▹",
+          "▹▹▹▸▹",
+          "▹▹▹▹▸"
+        ]
+      },
+      triangle: {
+        frames: %w{◢ ◣ ◤ ◥}
+      },
+      arc: {
+        frames: %w{ ◜ ◠ ◝ ◞ ◡ ◟ }
+      },
+      pipe: {
+        frames: %w{ ┤ ┘ ┴ └ ├ ┌ ┬ ┐ }
+      },
+      bouncing: {
+        frames: [
+          "[    ]",
+          "[   =]",
+          "[  ==]",
+          "[ ===]",
+          "[====]",
+          "[=== ]",
+          "[==  ]",
+          "[=   ]"
+        ]
+      },
+      bouncing_ball: {
+        frames: [
+          "( ●    )",
+          "(  ●   )",
+          "(   ●  )",
+          "(    ● )",
+          "(     ●)",
+          "(    ● )",
+          "(   ●  )",
+          "(  ●   )",
+          "( ●    )",
+          "(●     )"
+        ]
+      },
+      box_bounce: {
+        frames: %w{ ▌ ▀ ▐ ▄  }
+      },
+      box_bounce_2: {
+        frames: %w{ ▖ ▘ ▝ ▗ }
+      },
+      star: {
+        frames: %w{ ✶ ✸ ✹ ✺ ✹ ✷ }
+      },
+      toggle: {
+        frames: %w{ ■ □ ▪ ▫ }
+      },
+      balloon: {
+        frames: %w{ . o O @ * }
+      },
+      balloon_2: {
+        frames: %w{. o O ° O o . }
+      },
+      flip: {
+        frames: '-◡⊙-◠'.freeze
+      }
     }
   end # Formats
 end # TTY
