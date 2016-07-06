9be046fa6acb0669273c2733de31e79bf8afce37
Martin Becker <mbeckerwork@gmail.com>
2016-07-06 11:37:15 +0000

    adds test spec for start method with block

--- spec/unit/start_spec.rb
+++ spec/unit/start_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe TTY::Spinner, '#start' do
 
     expect(spinner).to have_received(:spin).at_least(5).times
   end
+
+  it "allows method to accept block and to start spinning automatically" do
+    spinner = TTY::Spinner.new(output: output, interval: 100)
+    allow(spinner).to receive(:spin)
+    spinner.start "done" do
+      100000000.times do |n|
+        n * n
+      end
+    end
+    expect(spinner).to have_received(:spin).at_least(5).times
+  end
 end
