6476dbf23bf1b040b84be87ad8557f1afd5474b1
Martin Becker <mbeckerwork@gmail.com>
2016-07-11 13:41:20 +0000

    changes spec unit test description to better describe test

--- spec/unit/start_spec.rb
+++ spec/unit/start_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe TTY::Spinner, '#start' do
     expect(spinner).to have_received(:spin).at_least(5).times
   end
 
-  it "allows method to accept block and to start spinning automatically" do
+  it "accepts block and stop message and will wait and spin until provided block finishes executing" do
     spinner = TTY::Spinner.new(output: output, interval: 100)
     allow(spinner).to receive(:spin)
     spinner.start "done" do
