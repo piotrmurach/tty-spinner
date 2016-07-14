f844c0d3da174fe65fcfc9e2ad8943e125527bad
Piotr Murach <pmurach@gmail.com>
2016-07-14 22:55:03 +0200

    Add run example.

---
+++ examples/run.rb
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+require 'tty-spinner'
+
+# without block
+spinner = TTY::Spinner.new(":title :spinner ...", format: :pulse_3)
+
+def long_task
+  10000000.times do |n|
+    n * n
+  end
+end
+
+spinner.update(title: 'Task 1')
+spinner.run 'done' do
+  long_task
+end
+
+spinner.update(title: 'Task 2')
+spinner.run('done') { long_task }
