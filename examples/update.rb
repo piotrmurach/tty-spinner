235936a803ed6500d4b170f21e5b1b89a94aa523
Piotr Murach <pmurach@gmail.com>
2016-08-07 21:22:56 +0100

    Change spinning times.

--- examples/update.rb
+++ examples/update.rb
@@ -6,8 +6,8 @@ spinner = TTY::Spinner.new(":spinner :title", format: :pulse_3)
 
 spinner.update(title: 'task aaaaa')
 
-5.times { spinner.spin; sleep(0.1) }
+20.times { spinner.spin; sleep(0.1) }
 
 spinner.update(title: 'task b')
 
-5.times { spinner.spin; sleep(0.1) }
+20.times { spinner.spin; sleep(0.1) }
