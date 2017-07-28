1f3581c8e7252b3b53b4e3dd4bd0aac0b6fff925
Piotr Murach <pmurach@gmail.com>
2017-07-28 20:54:56 +0100

    Add multi spinners example

---
+++ examples/multi.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+require 'tty-spinner'
+
+spinners = TTY::Spinner::Multi.new
+
+sp1 = spinners.register "[:spinner] one"
+sp2 = spinners.register "[:spinner] two"
+sp3 = spinners.register "[:spinner] three"
+
+sp1.auto_spin
+sp2.auto_spin
+sp3.auto_spin
+
+sleep(2)
+
+sp1.stop
+sp2.stop
+sp3.stop
