e658d78ef7db91249d285adcd4b96394b8a00b0c
Piotr Murach <pmurach@gmail.com>
2017-08-06 12:48:57 +0100

    Add example of starting multi spinner with async jobs in ref to issue #20

---
+++ examples/multi/jobs.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+require 'tty-spinner'
+
+spinners = TTY::Spinner::Multi.new("[:spinner] top")
+
+spinners.register("[:spinner] one")   { |sp| sleep(2); sp.success('yes 2') }
+spinners.register("[:spinner] two")   { |sp| sleep(3); sp.error('no 2') }
+spinners.register("[:spinner] three") { |sp| sleep(1); sp.success('yes 3') }
+
+spinners.auto_spin
+
