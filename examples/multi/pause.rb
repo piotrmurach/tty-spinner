907f7115bdaa5a25f95cd997f3dac73100419b44
Piotr Murach <pmurach@gmail.com>
2017-08-08 23:39:28 +0100

    Add multi pause exmample in ref to issue #21

---
+++ examples/multi/pause.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+require 'tty-spinner'
+
+spinners = TTY::Spinner::Multi.new("[:spinner]")
+
+sp1 = spinners.register("[:spinner] one")
+sp2 = spinners.register("[:spinner] two")
+sp3 = spinners.register("[:spinner] three")
+
+spinners.auto_spin
+sp1.auto_spin
+sp2.auto_spin
+sp3.auto_spin
+
+sleep(1)
+
+spinners.pause
+
+sleep(1)
+
+spinners.resume
+
+sleep(1)
+
+sp1.stop
+sp2.stop
+sp3.stop
+spinners.stop
