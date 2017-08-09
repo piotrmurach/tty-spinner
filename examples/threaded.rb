5e56886b315d05152de9e3cc1a62da434a1c96c1
Piotr Murach <pmurach@gmail.com>
2017-08-09 21:59:29 +0100

    Add threaded example for single spinner in ref to issue #23

---
+++ examples/threaded.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+require 'tty-spinner'
+
+spinner = TTY::Spinner.new("[:spinner]")
+
+th1 = Thread.new { 10.times { spinner.spin; sleep(0.1) } }
+th2 = Thread.new { 10.times { spinner.spin; sleep(0.1) } }
+th3 = Thread.new { 10.times { spinner.spin; sleep(0.1) } }
+
+[th1, th2, th3].each(&:join)
+
+spinner.success
