81824c5bbe474e1e7057414f60f1e1168583f624
Piotr Murach <pmurach@gmail.com>
2017-09-04 22:50:09 +0200

    Add multi spinner manual basic example

---
+++ examples/multi/basic.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+require 'tty-spinner'
+
+spinners = TTY::Spinner::Multi.new
+
+sp1 = spinners.register "[:spinner] one"
+sp2 = spinners.register "[:spinner] two"
+sp3 = spinners.register "[:spinner] three"
+
+th1 = Thread.new { 20.times { sleep(0.2); sp1.spin }}
+th2 = Thread.new { 30.times { sleep(0.1); sp2.spin }}
+th3 = Thread.new { 10.times { sleep(0.3); sp3.spin }}
+
+[th1, th2, th3].each(&:join)
