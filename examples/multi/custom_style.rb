a4550830c0096156e579cece3a2dfeeeb992f1cf
Piotr Murach <pmurach@gmail.com>
2017-09-11 22:49:15 +0200

    Change examples in line with automatic top level spinner spinning

--- examples/multi/custom_style.rb
+++ examples/multi/custom_style.rb
@@ -15,19 +15,13 @@ sp1 = spinners.register "[:spinner] one"
 sp2 = spinners.register "[:spinner] two"
 sp3 = spinners.register "[:spinner] three"
 
-spinners.auto_spin
 sp1.auto_spin
 sp2.auto_spin
 sp3.auto_spin
 
 sleep(2)
-
 sp1.success
 sleep 1
 sp2.success
-
 sleep 1
-
 sp3.error
-
-spinners.error
--- examples/multi/pause.rb
+++ examples/multi/pause.rb
@@ -8,7 +8,6 @@ sp1 = spinners.register("[:spinner] one")
 sp2 = spinners.register("[:spinner] two")
 sp3 = spinners.register("[:spinner] three")
 
-spinners.auto_spin
 sp1.auto_spin
 sp2.auto_spin
 sp3.auto_spin
