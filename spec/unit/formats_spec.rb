9d54fdff836ae6a385e5c6b081cbf0cf28a30e8e
Jared Ning <jared@redningja.com>
2017-10-26 17:48:54 -0500

    Hound suggestions.

--- spec/unit/formats_spec.rb
+++ spec/unit/formats_spec.rb
@@ -1,11 +1,9 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 RSpec.describe TTY::Formats::FORMATS do
-
   TTY::Formats::FORMATS.each do |token, properties|
     specify "#{token} contains proper defaults properties" do
       expect(properties.keys.sort).to eq(%i[frames interval])
     end
   end
-
 end
