484fe7f2548f7438909fd938b83662e41e3ada8f
Jared Ning <jared@redningja.com>
2017-10-26 17:46:38 -0500

    Fix spin_4 `:interval` property typo. Add test.

---
+++ spec/unit/formats_spec.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+RSpec.describe TTY::Formats::FORMATS do
+
+  TTY::Formats::FORMATS.each do |token, properties|
+    specify "#{token} contains proper defaults properties" do
+      expect(properties.keys.sort).to eq(%i[frames interval])
+    end
+  end
+
+end
