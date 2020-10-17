913933776cbdbc4ba8be3fcad3e7bc08e48e6de0
Piotr Murach <pmurach@gmail.com>
2020-10-17 14:54:37 +0200

    Add logging example

---
+++ examples/log.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-spinner"
+
+spinner = TTY::Spinner.new("[:spinner] processing...", format: :bouncing_ball)
+
+ 10.times do |i|
+   spinner.log("[#{i}] Task")
+   sleep(0.1)
+   spinner.spin
+ end
+
+ spinner.success
